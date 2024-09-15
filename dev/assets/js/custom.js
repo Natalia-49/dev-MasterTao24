ff00fe8a8dd2feedb7b713adc8f3e29495052378
Natalia-49 <vysotskanf@ukr.net>
2024-09-15 23:40:18 +0300

    banner and js

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,89 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+     
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+     
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+     
+  });
+
+
+  const latte = {
+    type: "coffee drink",
+    price: "1.5$",
+    consist: {
+      espresso: 80,
+      milk: 220,
+    }
+  }
+  console.log(latte.consist.milk)
+  console.log(latte.price)
   
+
+  const Moschino = {
+    type: "Perfumed water",
+    brand: "moschino",
+    country: "Italy",
+    volume: "50ml",
+    price: 1741,
+    note: {
+      initial: "Magnolia, Tangerine, Apple",
+      Heart: "Jasmine petals, Peony, Currant",
+      End: "Amberwood, Musk, Sandalwood",
+    }
+  }
+  console.log(Moschino.brand +" " + Moschino.country)
+
+
+  const man = {
+    name: "Fedor",
+    old: 72,
+    profession: "pensioner",
+    children: {
+      daughter:{
+        name: "Taliana",
+        old: 49,
+        profession: "designer",
+        son1: {
+          name: "Alexandr",
+          old: 27,
+          profession: "programmer",
+        },
+        son2: {
+          name: "Dmytro",
+          old: 18,
+          profession: "student",
+        }
+      }
+    }
+  }
+  const woman = {
+    name: "Tatiana",
+    old: 71,
+    profession: "pensioner",
+    children: {
+      daughter:{
+        name: "Maria",
+        old: 38,
+        profession: "freelancer",
+        son: {
+          name: "Andrey",
+          old: 5,
+          profession: "preschooler",
+        },
+        daughter: {
+          name: "Arina",
+          old: 12,
+          profession: "schoolgirl",
+        }
+      }
+    }
+  }
+console.log(man.children.daughter.son2.name)
+console.log(woman.children.daughter.son)
